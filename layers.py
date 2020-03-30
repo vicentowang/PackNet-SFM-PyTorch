9d9a4c5787f1dd9c31f37cc3d48ac4d03b0e4a9d
vicentowang <49265512+vicentowang@users.noreply.github.com>
2020-03-30 10:41:50 +0800

    recoding the network structure according to the paper

--- layers.py
+++ layers.py
@@ -193,7 +193,27 @@ class Project3D(nn.Module):
         pix_coords = (pix_coords - 0.5) * 2
         return pix_coords, z.view(self.batch_size, 1, self.height, self.width)
 
-class DepthToSpace(nn.Module):
+
+
+
+
+
+
+
+
+def Space2Depth_O(x, r=2):
+    """Takes a BCHW tensor and returns a B(rC)(H/r)(W/r) tensor,
+    by concatenating neighbor spatial pixels as extra channels.
+    It is the inverse of nn.PixelShuffle (if you apply both sequentially you should get the same tensor)
+    Example r=2: A RGB image (C=3) becomes RRRRGGGGBBBB (C=12) and is downsampled to half its size
+    """
+    b, c, h, w = x.shape
+    out_channel = c * (r ** 2)
+    out_h, out_w = h // r, w // r
+    x = x.contiguous().view(b, c, out_h, r, out_w, r)
+    return x.permute(0, 1, 3, 5, 2, 4).contiguous().view(b, out_channel, out_h, out_w)
+
+class Space2Depth(nn.Module):
 
     def __init__(self, block_size):
         super().__init__()
@@ -201,11 +221,194 @@ class DepthToSpace(nn.Module):
 
     def forward(self, x):
         N, C, H, W = x.size()
-        x = x.view(N, self.bs, self.bs, C // (self.bs ** 2), H, W)  # (N, bs, bs, C//bs^2, H, W)
-        x = x.permute(0, 3, 4, 1, 5, 2).contiguous()  # (N, C//bs^2, H, bs, W, bs)
-        x = x.view(N, C // (self.bs ** 2), H * self.bs, W * self.bs)  # (N, C//bs^2, H * bs, W * bs)
+        x = x.view(N, C, H // self.bs, self.bs, W // self.bs, self.bs)  # (N, C, H//bs, bs, W//bs, bs)
+        x = x.permute(0, 3, 5, 1, 2, 4).contiguous()  # (N, bs, bs, C, H//bs, W//bs)
+        x = x.view(N, C *(self.bs ** 2), H // self.bs, W // self.bs)  # (N, C*bs^2, H//bs, W//bs)
+        return x
+    
+class Conv_3D(nn.Module):
+    def __init__(self, out_channels = 8, kernel_size = 3, in_channels = 1):
+        super().__init__()
+        self.conv_3d = torch.nn.Conv3d(in_channels, out_channels, kernel_size, padding = (kernel_size - 1)//2)
+
+    def forward(self, x):
+        x = x.float()
+        x = x.unsqueeze(1)
+        return self.conv_3d(x)
+
+class Reshape(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        N, D, C, H, W = x.shape
+        return x.view(N, D*C ,H , W)
+
+class Conv_2D(nn.Module):
+    def __init__(self, in_channels , out_channels , kernel_size = 3):
+        super().__init__()
+        self.conv_2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, padding = (kernel_size - 1)//2)
+
+    def forward(self, x):
+        return self.conv_2d(x)
+
+class Packing(nn.Module):
+
+    def __init__(self, in_channels, out_channels, LR_times = 2, kernel_size = 3, depth_3dconv = 8):
+        super().__init__()
+        self.space_to_depth = Space2Depth(LR_times)
+        self.conv_3d = Conv_3D(depth_3dconv, kernel_size)
+        self.reshape = Reshape()
+        self.conv_2d = Conv_2D((LR_times**2)*in_channels*depth_3dconv, out_channels, kernel_size)
+
+    def forward(self, x):
+        x = self.space_to_depth(x)
+        x = self.conv_3d(x)
+        x = self.reshape(x)
+        x = self.conv_2d(x)      
         return x
 
+class Depth2Space(nn.Module):
+
+    def __init__(self, block_size):
+        super().__init__()
+        self.depth_to_space = nn.PixelShuffle(block_size)
+
+    def forward(self, x):
+        return self.depth_to_space(x);
+    
+class UnPacking(nn.Module):
+
+    def __init__(self, in_channels, out_channels, HR_times = 2, kernel_size = 3, depth_3dconv = 8):
+        super().__init__()
+        self.conv_2d = Conv_2D(in_channels, (HR_times**2)*out_channels//depth_3dconv, kernel_size)
+        self.conv_3d = Conv_3D(depth_3dconv, kernel_size)
+        self.reshape = Reshape()
+        self.depth_to_space = Depth2Space(HR_times)
+        
+    def forward(self, x):
+        x = self.conv_2d(x)
+        x = self.conv_3d(x)
+        x = self.reshape(x)
+        x = self.depth_to_space(x)
+        return x
+  
+class Conv2d(nn.Module):
+    def __init__(self, in_channels , out_channels , kernel_size = 3, GN = 16):
+        super().__init__()
+        self.conv_2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, padding = (kernel_size - 1)//2)
+        self.group_norm = nn.GroupNorm(GN, out_channels)
+        self.elu = nn.ELU(inplace=True)
+        
+    def forward(self, x):
+        x = self.conv_2d(x)
+        x = self.group_norm(x)
+        x = self.elu(x)
+        return x
+    
+class InvDepth(nn.Module):
+    def __init__(self, in_channels , out_channels , kernel_size = 3):
+        super().__init__()
+        self.conv_2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, padding = (kernel_size - 1)//2)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x):
+        x = self.conv_2d(x)
+        x = self.sigmoid(x)
+        return x  
+    
+class ResidualBlock(nn.Module):   
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()       
+        self.conv2d_1 = torch.nn.Conv2d(in_channels, out_channels, 3, padding = 1)
+        self.conv2d_2 = torch.nn.Conv2d(out_channels, out_channels, 3, padding = 1)
+        self.conv2d_3 = torch.nn.Conv2d(out_channels, out_channels, 1)
+        self.group_norm = nn.GroupNorm(16, out_channels)
+        self.elu = nn.ELU(inplace=True)
+        self.dropout = nn.Dropout2d(p=0.5)
+  
+    def forward(self, x):
+       
+        residual = x
+        
+        out = self.conv2d_1(x)
+        out = self.elu(out)
+
+        out = self.conv2d_2(out)
+        out = self.elu(out)
+        
+        out = self.conv2d_3(out)
+        out = self.group_norm(out)
+        
+        out += residual     
+        out = self.elu(out)
+        out = self.dropout(out)
+        return out
+
+def Upsample(x):
+    """Upsample input tensor by a factor of 2
+    """
+    return F.interpolate(x, scale_factor=2, mode="nearest")   
+    
+class PackNetsEncoder(nn.Module):
+
+    def __init__(self, num_input_images=1):
+        super(PackNetsEncoder, self).__init__()
+        self.layer1 = nn.Sequential( Conv2d(3, 64, kernel_size = 5) )
+        self.layer2 = nn.Sequential( Conv2d(64, 64, kernel_size = 7), Packing(64, 64) )
+        self.layer3 = nn.Sequential( ResidualBlock(64,64),ResidualBlock(64,64), Packing(64, 64) )
+        self.layer4 = nn.Sequential( ResidualBlock(64,64),ResidualBlock(64,64), Packing(64, 128) )
+        self.layer5 = nn.Sequential( ResidualBlock(128,128),ResidualBlock(128,128),ResidualBlock(128,128), Packing(128, 256) )
+        self.layer6 = nn.Sequential( ResidualBlock(256,256),ResidualBlock(256,256),ResidualBlock(256,256), Packing(256, 512) )
+        #self.encoder_nets = nn.Sequential(self.layer1,self.layer2,self.layer3,self.layer4,self.layer5,self.layer6)
+        
+        self.layer7 =   nn.ModuleDict({ 'Unpacking': UnPacking(512, 256) , 'Conv2d': Conv2d(512, 512) })
+        self.layer8 =   nn.ModuleDict({ 'Unpacking': UnPacking(512, 256) , 'Conv2d': Conv2d(384, 256) })
+        self.layer9 =   nn.ModuleDict({ 'InvDepth' : InvDepth(256, 1) })
+        
+        self.layer10 =  nn.ModuleDict({ 'Unpacking': UnPacking(1, 64) , 'Conv2d': Conv2d(129, 128) })
+        self.layer11 =  nn.ModuleDict({ 'InvDepth' : InvDepth(128, 1) })
+        
+        self.layer12 =  nn.ModuleDict({ 'Unpacking': UnPacking(1, 64) , 'Conv2d': Conv2d(129, 64) })
+        self.layer13 =  nn.ModuleDict({ 'InvDepth' : InvDepth(64, 1) })
+        
+        self.layer14 =  nn.ModuleDict({ 'Unpacking': UnPacking(1, 64) , 'Conv2d': Conv2d(129, 64) })
+        self.layer15 =  nn.ModuleDict({ 'InvDepth' : InvDepth(64, 1) })
+  
+    def forward(self, x0):
+        x1 = self.layer1(x0)
+        x2 = self.layer2(x1)
+        x3 = self.layer3(x2)
+        x4 = self.layer4(x3)
+        x5 = self.layer5(x4)
+        x6 = self.layer6(x5) # x6[N ,512, H/32， W/32]
+        
+        x7 = self.layer7['Conv2d'](torch.cat( (x5,self.layer7['Unpacking'](x6)) , 1))
+        x8 = self.layer8['Conv2d'](torch.cat( (x4,self.layer8['Unpacking'](x7)) , 1))
+        x9 = self.layer9['InvDepth'](x8)
+        
+        x10 = self.layer10['Conv2d'](torch.cat( (x3,self.layer10['Unpacking'](x9),Upsample(x9)) , 1))
+        x11 = self.layer11['InvDepth'](x10)
+        
+        x12 = self.layer12['Conv2d'](torch.cat( (x2,self.layer12['Unpacking'](x11),Upsample(x11)) , 1))       
+        x13 = self.layer13['InvDepth'](x12)
+        
+        x14 = self.layer14['Conv2d'](torch.cat( (x1,self.layer12['Unpacking'](x13),Upsample(x13)) , 1))
+        x15 = self.layer15['InvDepth'](x14)
+        
+        xout = x15
+        return xout # x15[N , 1, H ,W]
+
+
+
+
+
+
+
+
+
+
+  
 class SpaceToDepth(nn.Module):
 
     def __init__(self, block_size):
@@ -218,7 +421,19 @@ class SpaceToDepth(nn.Module):
         x = x.permute(0, 3, 5, 1, 2, 4).contiguous()  # (N, bs, bs, C, H//bs, W//bs)
         x = x.view(N, C *(self.bs ** 2), H // self.bs, W // self.bs)  # (N, C*bs^2, H//bs, W//bs)
         return x
+    
+class DepthToSpace(nn.Module):
 
+    def __init__(self, block_size):
+        super().__init__()
+        self.bs = block_size
+
+    def forward(self, x):
+        N, C, H, W = x.size()
+        x = x.view(N, self.bs, self.bs, C // (self.bs ** 2), H, W)  # (N, bs, bs, C//bs^2, H, W)
+        x = x.permute(0, 3, 4, 1, 5, 2).contiguous()  # (N, C//bs^2, H, bs, W, bs)
+        x = x.view(N, C // (self.bs ** 2), H * self.bs, W * self.bs)  # (N, C//bs^2, H * bs, W * bs)
+        return x
 # class ReflectionPad3d(nn.Module):
 #     ''' Implements 3d version of ReflectionPad2d'''
 #     def __init__(self, padding):
@@ -417,3 +632,22 @@ def compute_depth_errors(gt, pred):
     sq_rel = torch.mean((gt - pred) ** 2 / gt)
 
     return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
+
+    
+def testing():
+    x = torch.from_numpy(np.linspace(1,192,192))
+    x = x.view(1,3,8,8)
+
+    x = torch.randn(1, 3, 64, 64)
+    x = x.float()
+    pack = PackNetsEncoder()
+    out = pack(x)
+    net = Packing(3, 12)
+    for parameter in net.parameters():
+        print(parameter.shape)
+        
+    x1 = Packing(3, 12)(x)
+    x2 = UnPacking(12, 4)(x1)
+    return x2
+if __name__ == "__main__":
+    print(testing().shape)
